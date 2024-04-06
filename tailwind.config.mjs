b4fa3b24008d008430eabaf5a1f1723ef6c06276
JAguVillar <jaguvillar@gmail.com>
2024-04-06 07:39:50 -0300

    avances

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
     extend: {
       colors: {
         // Este es el color dominante utilizado a lo largo del diseño del sitio web. A menudo se utiliza para elementos clave como encabezados, fondos o botones principales. El color primario establece el tono general y el estado de ánimo del sitio web. Es esencial elegir un color primario que refleje la identidad de la marca y que resuene con el propósito del sitio web.
-        "ospet-primary": "#fff",
+        "ospet-primary": "#f3d2c1",
+        "ospet-background": "#fef6e4",
         //boton
         "ospet-button": "#f582ae",
         // Los colores secundarios complementan al color primario y se utilizan para agregar profundidad y variedad al diseño. Estos colores suelen aplicarse a elementos como botones secundarios, bordes u otros acentos de diseño. Los colores secundarios ayudan a crear jerarquía visual y equilibrio dentro del diseño. Deben armonizar bien con el color primario al tiempo que ofrecen suficiente contraste para que los elementos clave destaquen.
